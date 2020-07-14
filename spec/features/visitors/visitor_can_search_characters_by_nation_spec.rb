8b58e6aef0d055559371f246f64a10632493f392
Michael Gallup <Gallup93@icloud.com>
2020-07-14 10:57:10 -0600

    Things went awesome. Two thumbs up. >:|

---
+++ spec/features/visitors/visitor_can_search_characters_by_nation_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+# RSpec.describe "visitor search index page" do
+#   context "as a visitor" do
+#     visit "/"
+#
+#     within ".nav navbar-nav" do
+#
+#   end
+# end
